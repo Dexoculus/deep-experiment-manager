0860d8e355cb50a6cd229df87fe0d3b06cebba23
dexoculus <kirby5122@gmail.com>
2024-12-23 01:35:32 +0900

    Setups for TESTPYPI release

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ExperimentManager',
+    name='PytorchExperimentManager',
     version='0.1.0',
-    description='A module for training, testing, and managing experiments in PyTorch.',
     author='Dexoculus',
     author_email='hyeonbin@hanyang.ac.kr',
+    description='A module for training, testing, and managing experiments in PyTorch.',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Dexoculus/PyTorch-Experiment-Manager",
     packages=find_packages(),
-    install_requires=[
-        'torch',
-        'torchvision',
-        'PyYAML',
-        'matplotlib',
-
-    ],
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    install_requires=[
+        "tqdm>=4.64.0",
+        "pyyaml>=6.0",
+        "torch==2.4.0+cu124",
+        "torchvision==0.15.2",
+        "numpy>=1.26.4",
+        "scikit-learn==1.5.1",
+        "matplotlib>=3.8.0",
     ],
 )
