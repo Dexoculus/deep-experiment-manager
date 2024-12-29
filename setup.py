1188cdecad024c1a8df156d89cf017ed56b00b50
Dexoculus <34956179+Dexoculus@users.noreply.github.com>
2024-12-30 05:55:21 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='DeepExperimentManager',
-    version='0.1.2',
+    version='0.1.3',
     author='Dexoculus',
     author_email='hyeonbin@hanyang.ac.kr',
     description='A module for training, testing, and managing experiments in PyTorch.',
